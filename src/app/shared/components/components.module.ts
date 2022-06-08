1f1ab58c391125c65a934c7b2fe45b2ea32bb226
Paulo Gustavo <gustavo.pgvi@gmail.com>
2022-06-08 17:06:51 -0300

    feat: adicionado header e footer

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-	declarations: [
-		SidebarComponent,
-	],
-	exports: [
-		SidebarComponent,
-	],
-	imports: [
-		CommonModule,
-	],
+	declarations: [SidebarComponent, HeaderComponent, FooterComponent],
+  exports: [SidebarComponent, HeaderComponent, FooterComponent],
+	imports: [CommonModule],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
