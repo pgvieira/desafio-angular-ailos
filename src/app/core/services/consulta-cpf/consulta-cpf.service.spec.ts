6965df2a1d64429a0162a971d73b2bfacc3904cb
Paulo Gustavo <gustavo.pgvi@gmail.com>
2022-06-09 16:22:33 -0300

    feat: ajustando identação dos arquivos

--- src/app/core/services/consulta-cpf/consulta-cpf.service.spec.ts
+++ src/app/core/services/consulta-cpf/consulta-cpf.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { ConsultaCpfService } from './consulta-cpf.service';
 
 describe('ConsultaCpfService', () => {
-  let service: ConsultaCpfService;
+	let service: ConsultaCpfService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ConsultaCpfService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(ConsultaCpfService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
